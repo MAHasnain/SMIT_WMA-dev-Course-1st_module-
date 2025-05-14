f3a71913c0975c623567fd46a8e7bdcd49074ccc
Muhammad_Atta_Ul_Hasnain <attaulhasnainmaqsoodi@gmail.com>
2025-05-14 07:31:09 +0500

    html structure of 2nd page almost done

---
+++ JS_All_Assignments/app.js
@@ -0,0 +1,39 @@
+var myName = "MAHasnain";
+var isMarried = false;
+var num;
+
+console.log(typeof isMarried);
+console.log(typeof myName);
+console.log(myName);
+console.log(num);
+
+num = 427;
+
+// Math expressions:
+
+var num1 = 3;
+var num2 = 6;
+
+// console.log(num1 + num2);
+
+// console.log(num1 - num2);
+// console.log(num1 % num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// Math expressions:
+// Unfamiliar operators
+// increament and decreament
+
+// Preincreament
+
+
+// console.log(++num1);
+console.log(++num1);
+
+// Postincreament
+// console.log(num1++);
+// console.log(num1);
+
+
+// Predecreament
